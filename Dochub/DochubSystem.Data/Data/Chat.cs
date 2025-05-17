13644cf049311259c946d911bf42774ab4fb7802
voleducanh93 <voleducanh93@gmail.com>
2025-05-17 14:22:19 +0700

    setup

---
+++ Dochub/DochubSystem.Data/Data/Chat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DochubSystem.Data.Data
+{
+    public class Chat
+    {
+        public int ChatId { get; set; }
+        public int AppointmentId { get; set; }
+        public int UserId { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public Appointment Appointment { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Dochub/DochubSystem.Data/Data/Doctor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DochubSystem.Data.Data
+{
+    public class Doctor
+    {
+        public int DoctorId { get; set; }
+        public int UserId { get; set; }
+        public string Specialization { get; set; }
+        public int Experience { get; set; }
+        public string LicenseNumber { get; set; }
+
+        public User User { get; set; }
+        public ICollection<Appointment> Appointments { get; set; }
+    }
+}
